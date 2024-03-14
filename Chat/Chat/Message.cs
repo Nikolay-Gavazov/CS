7d0e9317a5f1b8a0928ade053de0255a3a680c1e
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-03-14 23:21:46 +0200

    Create Message.cs

---
+++ Chat/Chat/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Message
+    {
+        public Human Sender { get; }
+        public Human Receiver { get; }
+        public string Content { get; }
+
+        public Message(Human sender, Human receiver, string content)
+        {
+            Sender = sender;
+            Receiver = receiver;
+            Content = content;
+        }
+    }
+}
