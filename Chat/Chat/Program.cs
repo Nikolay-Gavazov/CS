ab781fd185d0db61b9d20191725498e0ab856c71
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-03-15 00:38:23 +0200

    Create Program.cs

---
+++ Chat/Chat/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Teacher teacher = new Teacher("Dimitrov", "prof.");
+  
+
+            Student student = new Student("Georgiev", "student");
+
+            teacher.SendMessage(student, "Hi Ivan Georgiev. Please send me the course work.");
+            teacher.SendMessage(student, "The deadline is till the end of the mounth.");
+            student.SendMessage(teacher, "Hi prof. Dimitrov, I send it 2 days ago. Please check in the spam also.");
+            teacher.SendMessage(student, "Hi Ivan Georgiev!I found it! You past the course with 6!");
+            student.SendMessage(teacher, "Hi prof. Dimitrov! Thanks a lot!");
+
+            ChatHistory.PrintChats();
+        }
+    }
+}
