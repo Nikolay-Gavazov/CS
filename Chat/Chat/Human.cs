b463024b474ac5c45fd0ef03431e2f4efe76de1c
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-03-14 23:21:42 +0200

    Create Human.cs

---
+++ Chat/Chat/Human.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Human
+    {
+        public string Name { get; set; }
+        public string Titel { get; set; }
+
+        public Human(string name, string titel)
+        {
+            Name = name;
+            Titel = titel; 
+        }
+
+        public void SendMessage(Human recipient, string message)
+        {
+            Message msg = new Message(this, recipient, message);
+            ChatHistory.AddMessage(msg);
+        }
+    }
+}
