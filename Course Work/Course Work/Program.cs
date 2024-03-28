d453777c074c427601ad27fce403914629e44d63
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-03-29 00:14:00 +0200

    Create Program.cs

---
+++ Course Work/Course Work/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Course_Work
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int limit = 4000000;
+            int sum = 0;
+            int first = 1;
+            int second = 2;
+
+            while (second <= limit)
+            {
+                if (second % 2 == 0)
+                {
+                    sum += second;
+                }
+
+                int next = first + second;
+                first = second;
+                second = next;
+            }
+            string result = "The sum of even-valued terms in the Fibonacci sequence is: ";
+            Console.WriteLine(result + sum);
+        }
+
+    }
+}
