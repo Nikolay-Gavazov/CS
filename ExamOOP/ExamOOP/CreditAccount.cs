62789007e804fa56c48f7be58dc282a508900ed7
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-04-12 17:57:40 +0300

    Create CreditAccount.cs

---
+++ ExamOOP/ExamOOP/CreditAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamOOP
+{
+    public class CreditAccount : SavingsAccount
+    {
+        public CreditAccount(string name, decimal balance) : base(name, balance)
+        {  
+
+        }
+
+        public override void GetCredit(decimal creditAmount, Customer obj)
+    {
+        decimal totalCreditAmount;
+        if(obj.age >= 65)
+            {
+                totalCreditAmount = creditAmount + creditAmount * (decimal)0.06;
+            }
+            else
+            {
+                totalCreditAmount = creditAmount + creditAmount * (decimal)0.08;
+            }
+        Console.WriteLine($"The Credit amount is ${totalCreditAmount} and the person is {obj.firstName} ");
+    }
+}
+}
