73ac80aaed7f5e6b1e2ed9cee2a93923e47c7db7
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-04-13 23:52:36 +0300

    Create Customer.cs

---
+++ ExamOOP/ExamOOP/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamOOP
+{
+    public class Customer
+    {
+        public int age = 55;
+        public string firstName = "Ivan";
+
+        public Customer(int age, string firstName)
+        {
+            this.age = age;
+            this.firstName = firstName;
+        }
+    }
+}
