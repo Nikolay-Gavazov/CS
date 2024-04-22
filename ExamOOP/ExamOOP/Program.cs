ec2a7a86bc714ebcac8f40d0369aecdc1796dbbe
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-04-22 11:11:19 +0300

    Create Program.cs

---
+++ ExamOOP/ExamOOP/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ExamOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SavingsAccount sa1 = new SavingsAccount("Ivan", 10000);
+            CreditAccount ca1 = new CreditAccount("Mitko", 10000);
+            Customer Petar = new Customer(65, "Petar");
+            Customer Dimitar = new Customer(45, "Dimitar");
+
+            sa1.GetCredit(1000, Dimitar);
+            ca1.GetCredit(4000, Petar);
+            
+        }
+    }
+}
