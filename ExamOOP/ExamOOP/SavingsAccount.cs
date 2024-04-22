a60b67221a67f7ae693fc6ec8aecddd5cbee1fdc
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-04-22 11:11:23 +0300

    Create SavingsAccount.cs

---
+++ ExamOOP/ExamOOP/SavingsAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamOOP
+{
+    public class SavingsAccount
+    {
+        protected string owner;
+        protected decimal balance;
+
+        public SavingsAccount(string name, decimal balance)
+        {
+            this.owner = name;
+            this.balance = balance;
+        }
+
+        public virtual void GetCredit(decimal creditAmount, Customer obj)
+        {
+            decimal totalCreditAmount;
+
+            totalCreditAmount = creditAmount + creditAmount * (decimal)0.08;
+            Console.WriteLine($"The Credit amount is ${totalCreditAmount} and the person is {obj.firstName} ");
+        }
+
+
+    }
+}
