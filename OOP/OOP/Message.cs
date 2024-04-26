0521b02ee65374b5815b275cc27f157c9d10c263
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-04-26 21:46:42 +0300

    Create Message.cs

---
+++ OOP/OOP/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Message
+    {
+        public Person Sender { get; }
+        public Person Receiver { get; }
+        public string Content { get; }
+
+        public Message(Person sender, Person receiver, string content)
+        {
+            this.Sender = sender;
+            this.Receiver = receiver;
+            this.Content = content;
+        }
+    }
+}
