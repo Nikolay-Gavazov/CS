5ff960bbb7d8243420d1f01937764d73f12b0eb6
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-04-26 21:46:29 +0300

    Create Archive.cs

---
+++ OOP/OOP/Archive.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Archive
+    {
+        private static List<Message> messages = new List<Message>();
+
+        public static void AddMessage(Message message)
+        { 
+            messages.Add(message);
+            Console.WriteLine("Message is send!");
+        }
+
+        public static void DisplayArchive()
+        {
+            Console.WriteLine("Message Archive:");
+            foreach (Message message in messages)
+            {
+                Console.WriteLine($"from: {message.Sender.Name}, to: {message.Receiver.Name}, Content: {message.Content}");
+            }
+        }
+    }
+}
