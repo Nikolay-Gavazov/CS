279de1576e9f816e5254539a164c27610b564bd5
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-05-05 00:55:48 +0300

    Create Person.cs

---
+++ OOP/OOP/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+
+        public void SendMessage(Person recipient, string message)
+        {
+            Message msg = new Message(this, recipient, message);
+            Archive.AddMessage(msg);
+        }
+    }
+}
