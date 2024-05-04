be9655f4851a10b4c45114706c017ae9b771046e
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-05-05 00:55:54 +0300

    Create Program.cs

---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Teacher teacher = new Teacher("prof. Ivanov");
+  
+
+            Student student = new Student("Petur Petrov");
+
+            teacher.SendMessage(student, "Hi Petur Petrov! You past the exam with 6!");
+            student.SendMessage(teacher, "Hi prof. Ivanov! Thanks a lot!");
+
+            Archive.DisplayArchive();
+        }
+    }
+}
